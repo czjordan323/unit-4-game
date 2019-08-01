5b77ec9449bc80ad321017a566c2e3557a37279f
lax <czjordan323@gmail.com>
2019-08-01 00:30:27 -0700

    created variables and linked js to html

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+var crystal = {
+    blue:
+    {
+        name: "lue",
+        value: 0
+    },
+    green: {
+      name: "Green",
+      value: 0  
+    },
+    red: {
+        name: "Red",
+        value: 0
+    },
+    yellow: {
+        name: "Yellow",
+        value: 0
+    },
+
+};
+
+var yourScore = 0;
+var scoreGoal = 0;
+
+var winCount = 0;
+var lossCount = 0;
+
